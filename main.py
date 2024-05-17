906ab582f97ab81a57f7731573b7e40995efc664
Keith Jung <60eokk@gmail.com>
2024-05-17 16:27:00 -0500

    q4 solve

--- main.py
+++ main.py
@@ -53,3 +53,20 @@ class Solution:
         return max_num
         
 
+
+# 4
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <=1:
+            return s
+
+        max_num = 1
+        max_str = s[0]
+        
+        for i in range(len(s)-1):
+            for j in range(i+1, len(s)):
+                if j-i+1 > max_num and s[i:j+1] == s[i:j+1][::-1]:
+                    max_num = j-i+1
+                    max_str = s[i:j+1]
+        
+        return max_str

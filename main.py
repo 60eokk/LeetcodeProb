e031358fae116a69341dd16bccd8a100887c118f
Keith Jung <60eokk@gmail.com>
2024-05-20 16:56:57 -0500

    q12 solve

--- main.py
+++ main.py
@@ -171,3 +171,45 @@ class Solution:
 
 # Q11
 # For efficiency, for loop inside for loop: O(n^2), a while loop is O(n). 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+
+        left = 0
+        right = len(height)-1
+        maxArea = 0
+
+        
+        while left < right:
+            currentArea = min(height[left], height[right]) * (right-left)
+            maxArea = max(currentArea, maxArea)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return maxArea
+
+
+# Q12
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        romanNumeral = {
+            1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX",
+            10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC",
+            100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM",
+            1000: "M", 2000: "MM", 3000: "MMM"
+            }
+
+        res = ""
+
+        if num in romanNumeral:
+            return romanNumeral[num]
+
+        else:
+            while num:
+                divisor = 10** (len(str(num))-1)
+                check = divisor * (num//divisor)
+                res += romanNumeral[check]
+                num = num % divisor
+            return res

a699196ed540f0752dcfd5c55a2e868f6911e4b4
Keith Jung <60eokk@gmail.com>
2024-05-20 15:45:22 -0500

    q9 solve

--- main.py
+++ main.py
@@ -160,3 +160,10 @@ class Solution:
 
         return ans
 
+# Q9
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if len(str(x)) == 1:
+            return True
+        for i in range(len(str(x))):
+            return True if str(x) == str(x)[::-1] else False

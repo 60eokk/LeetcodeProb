b4e9eef818bce4490a38342e542171c15ab8d64c
Keith Jung <60eokk@gmail.com>
2024-05-22 16:41:20 -0500

    q14 solve

--- main.py
+++ main.py
@@ -254,4 +254,14 @@ class Solution:
 # Q14
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        
+        strs = sorted(strs)
+        first = strs[0]
+        last = strs[-1]
+        ans = ""
+
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                return ans
+            ans += first[i]
+            
+        return ans

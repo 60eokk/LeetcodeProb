8eb8a9e7c7d4bfc0e118cdaeb5e78f61bd7615b4
Keith Jung <60eokk@gmail.com>
2024-05-22 17:30:17 -0500

    q15 solve

--- main.py
+++ main.py
@@ -283,3 +283,30 @@ for i in range(10):
     if i == 5:
         break  # Exit the loop when i is 5
     print(i)
+
+# back to question
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        newList = []
+        nums.sort()
+        
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            j = i+1
+            k = len(nums)-1
+
+            while j<k:
+                total = nums[i] + nums[j] + nums[k]
+                if total > 0:
+                    k-=1
+                elif total < 0:
+                    j+=1
+                else:
+                    newList.append([nums[i], nums[j], nums[k]])
+                    j+=1
+
+                    while nums[j] == nums[j-1] and j<k:
+                        j+=1
+        return newList

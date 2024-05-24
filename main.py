34c37b2579e0ca5d7f5e13b25f8019f3da02618a
Keith Jung <60eokk@gmail.com>
2024-05-24 15:00:52 -0500

    q18 solve

--- main.py
+++ main.py
@@ -363,3 +363,22 @@ class Solution:
 # Q18
 # vector: dynamic array mostly used in C++
 # THIS ONE IS HARD! TAKING A WHILE
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        ans = set()
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                k,l = j+1,len(nums)-1
+                while k<l:
+                    s = nums[i]+nums[j]+nums[k]+nums[l]
+                    if s == target:
+                        ans.add((nums[i],nums[j],nums[k],nums[l]))
+                        l-=1
+                        k+=1
+                    elif s > target:
+                        l-=1
+                    else:
+                        k+=1
+                  
+        return ans

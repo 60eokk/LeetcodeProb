6d67e0d3c83b708d3b27516cf64e520d21576f9a
Keith Jung <60eokk@gmail.com>
2024-05-28 14:28:38 -0500

    q22 solve

--- main.py
+++ main.py
@@ -439,3 +439,18 @@ class Solution:
 
 # Q22
 # Always think about "backtracking" in questions itself involves some kind of iteration
+class Solution(object):
+    def generateParenthesis(self, n):
+        def Generate_Parentheses(sentence, left, right):
+            if len(sentence) == n * 2:
+                l.append(sentence)
+                return
+            if left < n:
+                Generate_Parentheses(sentence + '(', left + 1, right)
+            if right < left:
+                Generate_Parentheses(sentence + ')', left, right + 1)
+
+        l = []
+        Generate_Parentheses('',0,0)
+        return l
+# it only adds a "right" only if the number of "left" is less than "right" --> which is the correct logic

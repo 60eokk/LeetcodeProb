7aa0fff67047cf6d48cafbf0b0180c6c4502b386
Keith Jung <60eokk@gmail.com>
2024-05-31 09:35:52 -0500

    q28 solve

--- main.py
+++ main.py
@@ -509,3 +509,11 @@ class Solution:
                 nums[count] = nums[i]
                 count +=1
         return count
+
+# Q28
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1

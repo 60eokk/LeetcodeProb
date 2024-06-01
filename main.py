9825825c5ded2c2dde41d7ac0804590d458c2c9d
Keith Jung <60eokk@gmail.com>
2024-05-31 19:10:24 -0500

    q29 solve

--- main.py
+++ main.py
@@ -523,11 +523,15 @@ class Solution:
 # 29
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
+        sign = -1 if (dividend >= 0 and divisor < 0) or (dividend < 0 and divisor >= 0) else 1
         dividend = abs(dividend)
         divisor = abs(divisor)
-        count = 0
-        temp = float('inf')
-        while temp > divisor:
-            temp = dividend - divisor
-            count += 1
-        return count
+        result = len(range(0, dividend-divisor+1, divisor))
+        if sign == -1:
+            result = -result
+        minus_limit = -(2**31)
+        plus_limit = (2**31 - 1)
+        result = min(max(result, minus_limit), plus_limit)
+        return result
+# encountered a lot of time limit exceeded errors. 
+# managed to solve by thinking of another algorihtm and keeping the min and max nums

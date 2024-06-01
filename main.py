219d25be2395d1def372f43967081a460e5dfe4c
Keith Jung <60eokk@gmail.com>
2024-06-01 09:38:27 -0500

    q33 solve

--- main.py
+++ main.py
@@ -570,3 +570,29 @@ class Solution:
                 return i
         else:
             return -1
+# above solution doesn't meet the requirements of solving in O(logn)
+# In order to solve this in O(logn) rutime complexity, use "modified binary search"
+# Think of "mid point"--> check left and right to see if it is ordered normally and whether target is in that half
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid]==target:
+                return mid
+            
+            # check if left half is normally ordered
+            if nums[left] <= nums[mid]:
+                # target is in left half
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # right half is normally ordered
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid +1
+                else:
+                    right =mid-1
+        return -1

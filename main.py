b4fe4356e627cf8dd3be1db9a8be41e408e86106
Keith Jung <60eokk@gmail.com>
2024-06-01 08:58:33 -0500

    q31 solve

--- main.py
+++ main.py
@@ -540,3 +540,23 @@ class Solution:
 # Q31
 # tricky part about this was that the function did not return anything
 # instead, change the nums itself, without using any extra spaces
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        i = len(nums) -2
+        while i>=0 and nums[i] >= nums[i+1]:
+            i -=1
+
+        if i==-1:
+            nums.reverse()
+        else:
+            pos = i+1
+            for j in range(len(nums)-1, i, -1):
+                if nums[j] > nums[i] and nums[j] < nums[pos]:
+                    pos =j
+
+            nums[i], nums[pos] = nums[pos], nums[i]
+            nums[i+1:] = sorted(nums[i+1:])

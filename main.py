e0921c460f15c6a5092849780d6df94824eda418
Keith Jung <60eokk@gmail.com>
2024-06-03 15:31:58 -0500

    q35 partially solved

--- main.py
+++ main.py
@@ -666,3 +666,18 @@ class Solution:
     
 
 # Q35
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+       
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+            
+        return left
+# above is the solution without the condition of "If not, return the index where it would be if it were inserted in order."

40697bd335d5fe63ac0096796625df2a9abceb04
Keith Jung <60eokk@gmail.com>
2024-06-03 15:02:19 -0500

    q34 solved. tough algorithm to think about

--- main.py
+++ main.py
@@ -599,3 +599,26 @@ class Solution:
     
 # Q34
 # Question is again asking to solve in O(logn) --> think of binary search
+class Solution:
+    def binarySearch(self, nums, target, side):
+        left, right = 0, len(nums) - 1
+        index = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if target > nums[mid]:
+                left = mid + 1
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                index = mid
+                if side == 0:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return index
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # '0' for left side, '1' for right side
+        left = self.binarySearch(nums, target, 0)
+        right = self.binarySearch(nums, target, 1)
+        return [left, right]

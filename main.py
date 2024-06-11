ffef206b6337b8db9da3fc5df4a00d9675b33f7d
Keith Jung <60eokk@gmail.com>
2024-06-11 15:36:33 -0500

    q56 solved

--- main.py
+++ main.py
@@ -1065,3 +1065,17 @@ class Solution:
 
 # Q56
 # lamda function is in this format --> lambda arguments: expression
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+
+        intervals.sort(key = lambda x: x[0])
+        merged = [intervals[0]]
+        for i in range(1,len(intervals)):
+            if merged[-1][1] >= intervals[i][0]:
+                merged[-1][1] = max(merged[-1][1], intervals[i][1])
+            else:
+                merged.append(intervals[i])
+
+        return merged

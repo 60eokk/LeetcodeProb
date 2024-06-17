54c25a194e879b6c78c429cd785d9f9976eed339
Keith Jung <60eokk@gmail.com>
2024-06-17 11:36:52 -0500

    q69 solve

--- main.py
+++ main.py
@@ -1301,3 +1301,19 @@ class Solution:
         if carry:
             res.append(carry)
         return ''.join(str(x) for x in res[::-1])
+
+# Q69
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 1, x
+        while left<=right:
+            mid = (left+right)//2
+
+            if mid*mid == x:
+                return mid
+            elif mid*mid <x:
+                left = mid+1
+            else:
+                right = mid-1
+        return right
+# easy binary search prob

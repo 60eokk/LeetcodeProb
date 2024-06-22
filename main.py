0e66ccc06e539af4be059929ddd1ed4bd86d43c7
Keith Jung <60eokk@gmail.com>
2024-06-22 14:49:54 -0500

    explanation for q75

--- main.py
+++ main.py
@@ -1494,17 +1494,22 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         zeros, ones, n = 0, 0, len(nums)
+        
+        # Count the number of 0s and 1s
         for num in nums:
             if num == 0:
                 zeros += 1
             elif num == 1:
                 ones += 1
 
+        # Fill the array with 0s for the first 'zeros' positions
         for i in range(0, zeros):
             nums[i] = 0
 
+        # Fill the array with 1s for the next 'ones' positions
         for i in range(zeros, zeros + ones):
             nums[i] = 1
 
+        # Fill the array with 2s for the remaining positions
         for i in range(zeros + ones, n):
             nums[i] = 2

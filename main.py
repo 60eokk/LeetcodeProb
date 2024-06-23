e4de0f721a7553cdfb553b4d23662f42f7214c9f
Keith Jung <60eokk@gmail.com>
2024-06-23 13:07:50 -0500

    q78 solve

--- main.py
+++ main.py
@@ -1531,3 +1531,27 @@ class Solution:
 
         backtrack(1, [])
         return res
+
+# Q78
+# For every number, there are 2 choice to include / or not include
+# Making a binary tree like going down from top to bottom
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return 
+            
+            # decision to include
+            subset.append(nums[i])
+            dfs(i+1)
+
+            # decision to NOT include
+            subset.pop()
+            dfs(i+1)
+
+        dfs(0)
+        return res

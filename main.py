5838521d4577e66a35dba9a9d50afab488e685c9
Keith Jung <60eokk@gmail.com>
2024-07-01 14:37:28 -0500

    q83 solved

--- main.py
+++ main.py
@@ -1657,4 +1657,15 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-# practice writing the ListNode func
+# practice writing the ListNode func (singly linked list)
+class Solution:
+    def deleteDuplicates(self, head):
+        current = head
+
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head

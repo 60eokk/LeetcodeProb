4f16ecb609c23a08b1198fee3638f8c893b0d929
Keith Jung <60eokk@gmail.com>
2024-07-12 11:11:53 -0500

    q98 solved

--- main.py
+++ main.py
@@ -1912,3 +1912,23 @@ class Solution:
 # Q98
 # The point of this problem is thinking which values to "compare", as we know that the node is between -inf, inf
 # When pointer goes down left, it will need to switch the upper bound to node.val, and when going right, it needs to switch lower bound to node.val
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(node, left, right):
+            if not node:
+                return True
+
+            if not (node.val < right and node.val > left):
+                return False
+
+            # now the recursive part
+            return (helper(node.left, left, node.val) and 
+            helper(node.right, node.val, right))
+
+        return helper(root, float("-inf"), float("inf"))

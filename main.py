05190afb081167e4d83b3b8ece3a321387de9014
Keith Jung <60eokk@gmail.com>
2024-07-15 13:08:11 -0500

    q102 solved

--- main.py
+++ main.py
@@ -2015,3 +2015,32 @@ class Solution:
 # Q102
 # Traverse level order: Breadst First Search!! (BFS)
 # Queue: First in First OUt (FIFO): Basically for this prob: Add by levels and then pop for each level from the queue
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+         # res.append()
+
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qlength = len(q)
+            level = []
+            for i in range(qlength):
+                node = q.popleft()
+
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+
+        return res

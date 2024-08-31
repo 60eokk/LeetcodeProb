6ec2e6c75b91b588a6a781139e4381b34881e09f
Keith Jung <60eokk@gmail.com>
2024-08-30 22:10:32 -0500

    q113 solved

--- main.py
+++ main.py
@@ -2281,3 +2281,23 @@ class Solution:
             return True
 
         return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
+
+
+# Q113
+class Solution:        
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if root is None:
+            return []
+        q, paths = deque([(root, targetSum, [])]), []
+        
+        while q:
+            cur, target, path = q.pop()  
+            if not (cur.left or cur.right) and cur.val == target:
+                paths.append(path + [cur.val])
+            else:
+                if cur.left:
+                    q.appendleft((cur.left, target - cur.val, path + [cur.val]))
+                if cur.right:
+                    q.appendleft((cur.right, target - cur.val, path + [cur.val]))
+                                 
+        return paths

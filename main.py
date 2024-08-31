158f2f8d70c7c0c72f85e3221025189a5884a07a
Keith Jung <60eokk@gmail.com>
2024-08-31 18:48:02 -0500

    q114 solved

--- main.py
+++ main.py
@@ -2301,3 +2301,24 @@ class Solution:
                     q.appendleft((cur.right, target - cur.val, path + [cur.val]))
                                  
         return paths
+
+
+# Q114
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        curr=root
+        while curr:
+            if curr.left!=None:
+                prev=curr.left
+                while prev.right:
+                    prev=prev.right
+                prev.right=curr.right
+                curr.right=curr.left
+                curr.left=None
+            curr=curr.right

4bf849531dd90499bf0124cb38920eae7d15029c
Keith Jung <60eokk@gmail.com>
2024-09-01 12:10:48 -0500

    q119 solved

--- main.py
+++ main.py
@@ -2346,3 +2346,20 @@ class Solution:
     
 
 # Q119
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        
+        row = [1]
+
+        for i in range(1, rowIndex+1):
+            new_row = [1]
+
+            for j in range(1,i):
+                new_element = row[j-1] + row[j]
+                new_row.append(new_element)
+            
+            new_row.append(1)
+
+            row = new_row
+        return row
+        

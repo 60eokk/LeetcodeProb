16cb7168c20a12397431e0d747ea8d276ecdfcfe
Keith Jung <60eokk@gmail.com>
2024-09-09 16:16:12 -0500

    q125 solved

--- main.py
+++ main.py
@@ -2405,3 +2405,18 @@ class Solution:
                 max += prices[i] - start
             start = prices[i]
         return max
+    
+
+# Q125
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalnum(): i += 1
+            while i < j and not s[j].isalnum(): j -= 1
+
+            if s[i].lower() != s[j].lower(): return False
+            i += 1
+            j -= 1
+
+        return True

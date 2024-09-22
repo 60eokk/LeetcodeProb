d3f9972ec4da1046db7724eb39e0bd6c891c6459
Keith Jung <60eokk@gmail.com>
2024-09-22 15:09:54 -0500

    q133 solved but not a good time complexity

--- main.py
+++ main.py
@@ -2544,6 +2544,43 @@ class Solution:
         return result
 
 
+# Q 133
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        
+        # Dictionary to keep track of cloned nodes
+        visited = {}
+        
+        # Define a helper function for DFS
+        def dfs(node):
+            if node in visited:
+                return visited[node]
+            
+            # Clone the node
+            clone = Node(node.val)
+            visited[node] = clone
+            
+            # Clone all the neighbors recursively
+            for neighbor in node.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+            
+            return clone
+        
+        # Start the DFS from the given node
+        return dfs(node)
+
+
 # Q134
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:

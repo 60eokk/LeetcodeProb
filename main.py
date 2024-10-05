a212d793f327cfa47fe9b8c359a5cc65772af304
Keith Jung <60eokk@gmail.com>
2024-10-04 22:25:04 -0500

    q139 solved

--- main.py
+++ main.py
@@ -2658,3 +2658,27 @@ class Solution:
             curr_new = curr_new.next
             
         return new_head
+    
+
+# Q139
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        def construct(current,wordDict, memo={}):
+            if current in memo:
+                return memo[current]
+
+            if not current:
+                return True
+
+            for word in wordDict:
+                if current.startswith(word):
+                    new_current = current[len(word):]
+                    if construct(new_current,wordDict,memo):
+                        memo[current] = True
+                        return True
+
+            memo[current] = False
+            return False
+
+        return construct(s,wordDict)
